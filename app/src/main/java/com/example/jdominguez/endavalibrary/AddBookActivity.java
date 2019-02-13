1d0d23230af9cb0d489d2ad32992dc7f30c4f053
Jorge Dominguez <jorge.dominguez@endava.com>
2019-02-13 18:25:30 -0500

    Added AddBookActivity class and its respective layout, started working on the buttons actions

---
+++ app/src/main/java/com/example/jdominguez/endavalibrary/AddBookActivity.java
@@ -0,0 +1,20 @@
+package com.example.jdominguez.endavalibrary;
+
+import android.os.Build;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class AddBookActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_book);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.backButtonToolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+    }
+}
